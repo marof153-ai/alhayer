596c821640cb6864c37fa4f92fc98d5286fd4377
marof153-ai <marof153@gmail.com>
2026-04-24 18:22:28 +0400

    Add gunicorn for render

--- alhayer_project/alhayer_project/settings.py
+++ alhayer_project/alhayer_project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_max_string=os.environ.get('DATABASE_URL'),
+        ssl_require=True,
+    )
