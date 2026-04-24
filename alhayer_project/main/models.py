71f45c33c243b2e102502e7b3f9fd492445d1e4f
marof153-ai <marof153@gmail.com>
2026-04-24 17:17:48 +0400

    Add gunicorn for render

--- alhayer_project/main/models.py
+++ alhayer_project/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):  # تأكد أن الكلمة تبدأ بحرف N كبير
+    title = models.CharField(max_length=200, verbose_name="العنوان")
+    content = models.TextField(verbose_name="المحتوى")
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100, verbose_name="الاسم")
+    email = models.EmailField(verbose_name="البريد الإلكتروني")
+    message = models.TextField(verbose_name="الرسالة")
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"رسالة من {self.name}"
--- alhayer_project/main/views.py
+++ alhayer_project/main/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import News, ContactMessage  # أضف الموديل الجديد هنا
+
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        # سحب البيانات من النموذج
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # حفظها في قاعدة البيانات
+        ContactMessage.objects.create(name=name, email=email, message=message)
+
+        # إعادة توجيه لنفس الصفحة لمنع تكرار الإرسال
+        return redirect('home')
+
+    news_list = News.objects.all().order_by('-date_posted')
+    return render(request, 'home.html', {'news_list': news_list})
