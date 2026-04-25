f4ebd678b6754aef86c1d481b32a066010a9e3a5
marof153-ai <marof153@gmail.com>
2026-04-25 10:57:04 +0400

    تعديل تحميل الصور

--- alhayer_project/alhayer_project/urls.py
+++ alhayer_project/alhayer_project/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls), # لوحة تحكم الإدارة الجاهزة
     path('', include('main.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # تغيير العنوان المتصدر في صفحة الدخول
 admin.site.site_header = "لوحة تحكم الهير الشرقية"
 
